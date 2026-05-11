0a1292e14dd719cf61f1bc5c17874b96aaa26d82
Nic <nic21vdw@gmail.com>
2026-05-11 15:14:35 -0400

    Add insights route

---
+++ src/app/insights/page.tsx
@@ -0,0 +1,10 @@
+import { DashboardInsightsPage } from "@/components/dashboard/insights-page";
+import { AppShell } from "@/components/layout/app-shell";
+
+export default function Page() {
+  return (
+    <AppShell>
+      <DashboardInsightsPage />
+    </AppShell>
+  );
+}
