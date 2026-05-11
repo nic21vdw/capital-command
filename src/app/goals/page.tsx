529a1427969a985544397d5b44edf404dccceaff
Nic <nic21vdw@gmail.com>
2026-05-11 15:14:34 -0400

    Add goals route

---
+++ src/app/goals/page.tsx
@@ -0,0 +1,10 @@
+import { GoalsPage } from "@/components/goals/goals-page";
+import { AppShell } from "@/components/layout/app-shell";
+
+export default function Page() {
+  return (
+    <AppShell>
+      <GoalsPage />
+    </AppShell>
+  );
+}
