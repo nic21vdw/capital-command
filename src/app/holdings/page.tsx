dc377a6ea2def903693509be5677f88af757c262
Nic <nic21vdw@gmail.com>
2026-05-11 15:14:29 -0400

    Add holdings route

---
+++ src/app/holdings/page.tsx
@@ -0,0 +1,10 @@
+import { HoldingsPage } from "@/components/holdings/holdings-page";
+import { AppShell } from "@/components/layout/app-shell";
+
+export default function Page() {
+  return (
+    <AppShell>
+      <HoldingsPage />
+    </AppShell>
+  );
+}
