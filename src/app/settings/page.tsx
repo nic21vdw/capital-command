8f1a0638a49c60b09d1d9afc47465e00532caff2
Nic <nic21vdw@gmail.com>
2026-05-11 15:14:37 -0400

    Add settings route

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,10 @@
+import { AppShell } from "@/components/layout/app-shell";
+import { SettingsPage } from "@/components/layout/settings-page";
+
+export default function Page() {
+  return (
+    <AppShell>
+      <SettingsPage />
+    </AppShell>
+  );
+}
