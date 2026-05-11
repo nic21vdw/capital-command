18b4e9b36fd4e3f1d53053b8bb17306c96e6cf42
Nic <nic21vdw@gmail.com>
2026-05-11 15:14:31 -0400

    Add watchlist route

---
+++ src/app/watchlist/page.tsx
@@ -0,0 +1,10 @@
+import { AppShell } from "@/components/layout/app-shell";
+import { WatchlistPage } from "@/components/watchlist/watchlist-page";
+
+export default function Page() {
+  return (
+    <AppShell>
+      <WatchlistPage />
+    </AppShell>
+  );
+}
